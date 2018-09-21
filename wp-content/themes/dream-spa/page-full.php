1e9a289004f49c1935f6405f63a3416dce39fe0d
vandoan <doanvanvandvv@gmail.com>
2018-09-21 09:24:42 +0700

    message

---
+++ wp-content/themes/dream-spa/page-full.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Page Full Width
+ *
+ */
+get_header();
+?>
+
+<!-- Blog & Sidebar Section -->
+<section id="section" class="section">		
+	<img src="http://spa.test/wp-content/uploads/2018/09/home_banner.jpg" alt="pandanus">
+	<div class="slide-content">
+		<h2>The Essence of Natural 
+		Beaty!</h2>
+		<p>We at Dream Spa provie various services to the nature of the clients!</p>
+		<button>Learn More</button>
+	</div>
+</section>
+<section class="chat-now">
+	<h2>Make Appointment</h2>
+	<p>Chat now</p>
+	ICON
+</section>
+<hr>
+<section class="service">
+	<h2>Amazing Service</h2>
+	<p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+	<div class="img-service">
+		<img src="http://spa.test/wp-content/uploads/2018/09/heading-boder.png" alt="">
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			a
+		</div>
+		<div class="col-md-4">
+			b
+		</div>
+		<div class="col-md-4">
+			c
+		</div>
+	</div>
+</section>
+<!-- End of Blog & Sidebar Section -->
+<div class="clearfix"></div>
+
+<?php get_footer(); ?>
